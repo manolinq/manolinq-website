9d040464752f6d9ce6069e2e7b26487f1539d13a
manolinq <manolito.smetje@gmail.com>
2026-05-30 12:11:22 +0200

    Update email in Contact & Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const footerLinks = [
 ];
 
 const contactLinks = [
-  { href: 'mailto:hello@manolinq.com',      icon: Mail,          label: 'hello@manolinq.com' },
+  { href: 'mailto:support.manolinq@gmail.com', icon: Mail,        label: 'support.manolinq@gmail.com' },
   { href: 'https://instagram.com/manolinq', icon: Instagram,     label: '@manolinq',         external: true },
   { href: 'https://wa.me/32456326720?text=Hey%2C%20I%27m%20interested%20in%20a%20website%20or%20AI%20system.%20Can%20you%20take%20a%20quick%20look%20at%20my%20business%3F', icon: MessageCircle, label: 'WhatsApp',          external: true },
 ];
